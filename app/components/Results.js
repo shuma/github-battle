096c787ff07446b77fd4614b8179d9794ae9f7b4
shuma <usman.rajab@gmail.com>
2017-05-24 18:49:07 +0200

    github api requests & react battle component

---
+++ app/components/Results.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Results extends React.Component {
+
+  render() {
+    console.log(this.props);
+    return (
+      <div>Results</div>
+    );
+  }
+
+}
+
+module.exports = Results;
